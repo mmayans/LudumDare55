ed80b8cb8677834a667d301826c5cfdd503e1879
Madeline Mayans <82738387+mmayans@users.noreply.github.com>
2024-04-12 19:08:54 -0700

    this is the movement

---
+++ Summoning/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+           transform.position = transform.position + new Vector3(0f, 0f, 0.5f);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+           transform.position = transform.position + new Vector3(0f, 0f, -0.5f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+           transform.position = transform.position + new Vector3(0.5f, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+           transform.position = transform.position + new Vector3(-0.5f, 0f, 0f);
+        }
+
+    }
+}
